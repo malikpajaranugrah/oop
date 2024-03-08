b0a271fd0aa340a7c6e972892e5ba7e708ccb468
malikpajaranugrah <114727888+malikpajaranugrah@users.noreply.github.com>
2024-03-08 13:38:58 +0700

    RightTA.java

--- Session3/RightTA.java
+++ Session3/RightTA.java
@@ -1,4 +1,13 @@
-public class RightTA {
+public class RightTA extends Triangle {
+    public RightTA (String name, double height, double base, double hypotenus) {
+        super(name, height, base, hypotenus);  
+    }
 
+    @Override
+    public double calculatearea() {
+        double luas = 0.5 * getsideB() * getsideA(); 
+        return luas;
+    }
 }
+
    
