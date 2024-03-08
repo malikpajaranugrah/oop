dbe26bfa46e468f8beafc002a7bd6b4ad5f3a7c2
malikpajaranugrah <114727888+malikpajaranugrah@users.noreply.github.com>
2024-03-08 13:34:48 +0700

    Circle.java

--- Session3/Circle.java
+++ Session3/Circle.java
@@ -1,3 +1,15 @@
-public class Circle {
-    
+public class Circle extends Shape{
+    public Circle(String name, double radius){
+        super(name);
+        calculateperimeter(radius);
+        calculatearea(radius);
+    }
+
+    public void calculateperimeter(double radius){
+        perimeter = 2 * Math.PI * radius;
+    }
+
+    public void calculatearea(double radius){
+        area = Math.PI * radius * radius;
+    }
 }
