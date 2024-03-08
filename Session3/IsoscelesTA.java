0cfe8a6334f6ea323076f6478a91737117c6e1df
malikpajaranugrah <114727888+malikpajaranugrah@users.noreply.github.com>
2024-03-08 13:36:10 +0700

    IsoscelesTA.java

--- Session3/IsoscelesTA.java
+++ Session3/IsoscelesTA.java
@@ -1,3 +1,6 @@
-public class IsoscelesTA {
-    
+public class IsoscelesTA extends Triangle {
+    public IsoscelesTA (String name, double hypotenus, double base) {
+        super(name, hypotenus,hypotenus, base);  
+    }
 }
+
